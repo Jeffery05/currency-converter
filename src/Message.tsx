6aca0c54cc1384be57e796b885db100a2821441b
Jeffery Hu <jefferyhu2005@gmail.com>
2023-03-25 10:59:00 -0400

    changed the tab label and cleaned up files

--- src/Message.tsx
+++
@@ -1,9 +0,0 @@
-
-//PascalCasing
-function Message () {
-    // JSX: JavaScript XML
-    const name = 'Currency Converter Tool'
-    return <h1>{name}</h1>
-}
-
-export default Message;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,3 @@
-import UserInput from "./Result";
-
 function Home() {
   return (
     <div>
